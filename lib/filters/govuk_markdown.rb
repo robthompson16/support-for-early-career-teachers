8fa2cc0b2c296354ddfefe5a7b88e46646a1ea35
Peter Yates <peter.yates@graphia.co.uk>
2023-10-17 10:57:39 +0100

    Make GovukMarkdown start with 'l' instead of 'xl'

--- lib/filters/govuk_markdown.rb
+++ lib/filters/govuk_markdown.rb
@@ -1,5 +1,5 @@
 require 'govuk_markdown'
 
 Nanoc::Filter.define(:govuk_markdown) do |content, _params|
-  GovukMarkdown.render(content)
+  GovukMarkdown.render(content, headings_start_with: "l")
 end
